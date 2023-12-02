c8a37a8887d589ce32206d1fac690527df885790
Vikas Malhotra <malhotra.vikas@gmail.com>
2023-12-02 00:56:24 -0500

    Code complete for download and upload

--- adminSite/photos/app.js
+++ adminSite/photos/app.js
@@ -7,7 +7,7 @@ const tableName = "recognizedTextEvent"; // Replace with your DynamoDB table nam
 // Define the attribute name for event name
 const binAttribute = "detectedText";
 const fotosAttribute = "imageArtifacts";
-
+checkedImages = [];
 
 // Add an event listener for the "DOMContentLoaded" event
 document.addEventListener("DOMContentLoaded", function() {
@@ -19,6 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("binForm").addEventListener("submit", handleFormSubmission);
 	//document.getElementById('downloadBinImagesButton').addEventListener('click', downloadBinImages);
 	document.getElementById('downloadAllButton').addEventListener('click', downloadAllImagesForBin);
+	document.getElementById('downloadSelectedButton').addEventListener('click', downloadSelectedImages);
+
 });
 
         // Function to show the spinner
@@ -137,9 +139,10 @@ function removeDuplicateImages(imageUrls) {
 }
 
 
-function downloadAllImagesForBin(imageUrls, bin) {
+function downloadAllImagesForBin(bin) {
 	showSpinner();
 
+	checkedImages = [];
 
     // Select all image elements within the photosContainer
     const images = document.querySelectorAll('#photosContainer a');
@@ -200,9 +203,61 @@ function downloadAllImagesForBin(imageUrls, bin) {
 	}
 
 
-// Function to fetch and display events
-function fetchAndDisplayFotos(binToSearch) {
+function downloadSelectedImages() {
+	showSpinner();
 
+    
+	console.log("Attempting to doaload selected images  " + checkedImages.length);
+
+	
+    // Create an array to store the fetch promises
+    const fetchPromises = [];
+
+    // Create a zip file to store the images
+    const zip = new JSZip();
+
+    // Iterate through the images and add them to the zip file
+    checkedImages.forEach((image, index) => {
+        const imageUrl = image;
+        console.log("Image URL to fetch is - " + imageUrl);
+		// Split the URL by '/' to get the parts
+		const parts = String(imageUrl).split('/');
+
+		// The last part (after the last '/') should be the image name
+		const imageName = parts[parts.length - 1];
+
+		console.log("Image Name:", imageName);
+
+        fetchPromises.push(
+	        fetch(imageUrl)
+	            .then(response => response.blob())
+	            .then(blob => {
+			      console.log("Downloading Image Name:", imageName);
+				  zip.file(imageName, blob);
+	            })
+	        );
+    });
+
+    // Wait for all fetch operations to complete
+    Promise.all(fetchPromises)
+        .then(() => {
+        	// Generate and trigger the download of the zip file
+		    zip.generateAsync({ type: 'blob' }).then(function(content) {
+		        const downloadLink = document.createElement('a');
+		        downloadLink.href = URL.createObjectURL(content);
+		        downloadLink.download = 'selection' + '.zip'; // Customize the zip file name if needed
+		        downloadLink.click();
+		        hideSpinner();
+    		});
+    	})
+	    .catch(error => {
+	            console.error("Error fetching images:", error);
+	        });
+
+	}
+
+// Function to fetch and display events with image thumbnails and checkboxes
+function fetchAndDisplayFotos(binToSearch) {
 	// Define the parameters for scanning the DynamoDB table
 	const params = {
 		TableName: tableName,
@@ -212,7 +267,7 @@ function fetchAndDisplayFotos(binToSearch) {
 		},
 	};
 
-	// Use the DynamoDB scan operation to retrieve all items from the table
+	// Use the DynamoDB query operation to retrieve all items from the table
 	dynamodb.query(params, (error, data) => {
 		if (error) {
 			console.error("Error fetching events from DynamoDB:", error);
@@ -222,57 +277,70 @@ function fetchAndDisplayFotos(binToSearch) {
 			// Clear the existing content inside the container
 			photosContainer.innerHTML = "";
 
-
 			const searchResultItems = data.Items;
 			console.log("Items with detectedText value :", searchResultItems);
 
 			if (searchResultItems && searchResultItems.length > 0) {
-				let index = 0;
-				
 				// Iterate through the retrieved items
 				searchResultItems.forEach((item) => {
 					// Access attributes by their attribute names
 					const detectedText = item.detectedText; // Replace with your actual attribute name
 					const imageArtifacts = item.imageArtifacts; // Replace with your actual attribute name
 
-					// You can now work with the values of the attributes
-					console.log("Detected Text:", detectedText);
-					console.log("Image Artifacts:", imageArtifacts);
-
-					// Split the imageArtifactsString into an array using ', ' as the delimiter
 					const imageArtifactsArray = imageArtifacts.split(', ');
-					// Create a new table for each set of URLs
-					const table = document.createElement("table");
-					table.className = "image-table"; // You can define CSS styles for this class
+					
+					// Check if imageArtifacts is an array
+					if (Array.isArray(imageArtifactsArray)) {
+						// Create a new div for the set of thumbnails and checkboxes
+						const thumbnailDiv = document.createElement("div");
+						thumbnailDiv.className = "thumbnail-container"; // You can define CSS styles for this class
 
-					// Iterate through the individual URLs in the array and create table rows
-					imageArtifactsArray.forEach((url) => {
-						const row = table.insertRow();
-						const cell = row.insertCell();
+						// Iterate through the individual URLs in the array and create thumbnails with checkboxes
+						imageArtifactsArray.forEach((url) => {
+							// Create a div for each thumbnail and checkbox
+							const thumbnailAndCheckboxDiv = document.createElement("div");
+							thumbnailAndCheckboxDiv.className = "thumbnail-checkbox-container"; // You can define CSS styles for this class
 
-						// Create an anchor element with the URL as the href attribute
-						const link = document.createElement("a");
-						link.href = url;
-						link.textContent = url;
+							// Create an image thumbnail
+							const thumbnail = document.createElement("img");
+							thumbnail.src = url;
+							thumbnail.width = 250;
+							thumbnail.height = 250;
 
-						cell.appendChild(link);
-						// You can apply additional styling or functionality to the cells as needed
-					});
+							// Create a checkbox
+							const checkbox = document.createElement("input");
+							checkbox.type = "checkbox";
+							
+							        // Add an event listener to checkboxes to track changes
+					        checkbox.addEventListener("change", function () {
+					            if (this.checked) {
+					                // If the checkbox is checked, add the image URL to the array
+					                checkedImages.push(url);
+					            } else {
+					                // If the checkbox is unchecked, remove the image URL from the array
+					                const index = checkedImages.indexOf(url);
+					                if (index !== -1) {
+					                    checkedImages.splice(index, 1);
+					                }
+					            }
+					        });
 
-					// Add the table to the container
-					photosContainer.appendChild(table);
+						    console.log("Checked Images:", checkedImages);
 
-					// Add a separator (optional) between tables
-					if (index < data.Items.length - 1) {
-						const separator = document.createElement("hr");
-						separator.className = "separator";
-						container.appendChild(separator);
+
+							// Append the thumbnail and checkbox to the container
+							thumbnailAndCheckboxDiv.appendChild(thumbnail);
+							thumbnailAndCheckboxDiv.appendChild(checkbox);
+
+							// Append the thumbnail and checkbox container to the main div
+							thumbnailDiv.appendChild(thumbnailAndCheckboxDiv);
+						});
+
+						// Add the set of thumbnails and checkboxes to the container
+						photosContainer.appendChild(thumbnailDiv);
+					} else {
+						console.log("imageArtifacts is not an array:", imageArtifactsArray);
 					}
-
-					//displayPhotos(imageArtifactsArray, 200, 150);
-
-
-					// Perform any other processing you need with the values
 				});
 			} else {
 				console.log("No items found with the specified detectedText value.");
@@ -280,6 +348,7 @@ function fetchAndDisplayFotos(binToSearch) {
 		}
 	});
 }
+
 
 // Function to trigger the download
 function downloadBinImages(imageUrls) {
@@ -332,6 +401,8 @@ function downloadBinImages(imageUrls) {
 // Function to handle form submission
 function handleFormSubmission(event) {
 	event.preventDefault();
+	checkedImages = [];
+
 
 	// Get the event name from the input field
 	const binName = document.getElementById("binName").value;
